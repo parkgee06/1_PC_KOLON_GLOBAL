041cf5c5a4c514c7b96646b42f93663cbdf86f4d
parkgee06 <parkgee06@gmail.com>
2024-08-12 12:38:26 +0900

    h3수정/gb폴더삭제

---
+++ gb/bbs/_head.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>디지털홍보관-공지사항</title>
+
+    <link rel="stylesheet" href="../../common/css/common.css">
+    <link rel="stylesheet" href="../../common/css/sub_common.css">
+    <link rel="stylesheet" href="../../sub6/common/css/sub6common.css">
+    <link rel="stylesheet" href="../../css/sub6_content1.css">
+    <style>
+        #headerArea #gnb{background:none}
+        #headerArea h3 {font-size: 1.17em;
+            font-family:"Noto Sans KR", sans-serif;}
+        #content{padding-bottom: 200px;}
+    </style>
+
+    <!-- 구글폰트 -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <!-- 폰트어썸 -->
+    <script src="https://kit.fontawesome.com/86dcaacab3.js" crossorigin="anonymous"></script>
+
+    <!-- 올릴때 아래의 주석 풀것 -->
+    <script src="../../common/js/prefixfree.min.js"></script>
+</head>
+<body>
+      <!-- 글로벌네비게이션 -->
+<div id="skipNav">
+    <a href="#content">본문바로가기</a>
+    <a href="#gnb">글로벌네비게이션바로가기</a>
+</div>
+
+<!-- header 공통영역 -->
+<div id="wrap">
+    <header id="headerArea">
+        <div class="header_container">
+            <h1 class="logo">
+                <a href="../../index.html">코오롱글로벌</a>
+            </h1>
+            
+            <nav id="gnb">
+                <h2 class="hidden">글로벌네비게이션</h2>
+                <ul class="dropdownmenu">
+                    <li class="m1 menu">
+                        <h3><a class="depth1" href="../../sub1/sub1_1.html">회사소개</a></h3>
+                        <ul>
+                            <li><a href="../../sub1/sub1_1.html">경영이념</a></li>
+                            <li><a href="../../sub1/sub1_2.html">기업연혁</a></li>
+                            <li><a href="../../sub1/sub1_3.html">안전보건경영</a></li>
+                            <li><a href="../../sub1/sub1_4.html">CI소개</a></li>
+                            <li><a href="../../sub1/sub1_5.html">오시는길</a></li>
+                        </ul>
+                    </li>
+                    <li class="m2 menu">
+                        <h3><a class="depth1" href="../../sub2/sub2_1.html">사업영역</a></h3>
+                        <ul>
+                            <li><a href="../../sub2/sub2_1.html">건설</a></li>
+                            <li><a href="../../sub2/sub2_2.html">상사</a></li>
+                            <li><a href="../../sub2/sub2_3.html">스포렉스</a></li>
+                        </ul>
+                    </li>
+                    <li class="m3 menu">
+                        <h3><a class="depth1" href="../../sub3/sub3_1.html">네트워크</a></h3>
+                        <ul>
+                            <li><a href="../../sub3/sub3_1.html">글로벌 네트워크</a></li>
+                            <li><a href="../../sub3/sub3_2.html">국내 네트워크</a></li>
+                        </ul>
+                    </li>
+                    <li class="m4 menu">
+                        <h3><a class="depth1" href="../../sub4/sub4_1.html">ESG</a></h3>
+                        <ul>
+                            <li><a href="../../sub4/sub4_1.html">ESG 경영</a></li>
+                            <li><a href="../../sub4/sub4_2.html">환경</a></li>
+                            <li><a href="../../sub4/sub4_3.html">사회</a></li>
+                            <li><a href="../../sub4/sub4_4.html">지배구조</a></li>
+                        </ul>
+                    </li>
+                    <li class="m5 menu">
+                        <h3><a class="depth1" href="../../sub5/sub5_1.html">인재채용</a></h3>
+                        <ul>
+                            <li><a href="../../sub5/sub5_1.html">인재상</a></li>
+                            <li><a href="../../sub5/sub5_2.html">인사제도</a></li>
+                            <li><a href="../../sub5/sub5_3.html">채용안내</a></li>
+                            <li><a href="../../sub5/sub5_4.html">채용문의</a></li>
+                        </ul>
+                    </li>
+                    <li class="m6 menu">
+                        <h3><a class="depth1" href="https://parkgee06.mycafe24.com/gb/bbs/board.php?bo_table=free">디지털홍보관</a></h3>
+                        <ul>
+                            <li><a href="https://parkgee06.mycafe24.com/gb/bbs/board.php?bo_table=free">공지사항</a></li>
+                            <li><a href="../../sub6/sub6_2.html">뉴스센터</a></li>
+                            <li><a href="../../sub6/sub6_3.html">e-브로셔</a></li>
+                            <li><a href="../../sub6/sub6_4.html">홍보동영상</a></li>
+                            <li><a href="../../sub6/sub6_5.html">포토갤러리</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+
+            <div class="top_menu">
+                <ul>
+                    <li><a href="../../member/member_check.html">회원가입</a></li>
+                    <li><a href="../../login/login.html">로그인</a></li>
+                </ul>
+            </div>                
+        </div>        
+    </header>
+      <!-- visual 영역 -->
+      <div class="visual">
+        <img src="../../sub6/common/images/sub_6_visual.png" alt="">
+        <span>디지털홍보관</span>
+        <p>코오롱글로벌의 다양한 이야기를 영상과 갤러리를 통해 만나보세요.</p>
+    </div>
+
+    <div class="sub_nav">
+        <ul>
+        <li><a href="https://parkgee06.mycafe24.com/gb/bbs/board.php?bo_table=free" class="current">공지사항</a></li>
+        <li><a href="../../sub6/sub6_2.html">뉴스센터</a></li>
+        <li><a href="../../sub6/sub6_3.html">e-브로셔</a></li>
+        <li><a href="../../sub6/sub6_4.html">홍보동영상</a></li>
+        <li><a href="../../sub6/sub6_5.html">포토갤러리</a></li>
+        </ul>
+    </div>
+
+
+<!-- 본문시작 -->
+<article id="content">
+    <section  class="content_area">
+    <div class="title_area">
+        <div class="line_map">
+            home &gt; 디지털홍보관 &gt; <strong>공지사항</strong>
+        </div>
+        <h2>디지털홍보관</h2>
+    </div>
+
+    <div  class="content_1 con">
+        <h3>공지사항</h3>
+        <span class="head3_en">notice</span>
+        <!-- 게시판 삽입 부분 -->
