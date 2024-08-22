52c55a394676109e3c135de24a95224063066e19
parkgee06 <parkgee06@gmail.com>
2024-08-22 17:52:52 +0900

    top값 조정

--- sub3/js/slide_move.js
+++ sub3/js/slide_move.js
@@ -28,23 +28,23 @@
                   var value=0; //이동할 스크롤의 거리
 
                   if($(this).hasClass('link1')){   //첫번째 메뉴를 클릭했냐?   if($(this).is('#link1')){
-                     value= $('#content .con1').offset().top;  // 해당 콘테츠의 상단의 거리~~
+                     value= $('#content .con1').offset().top -180;  // 해당 콘테츠의 상단의 거리~~
                   }else if($(this).hasClass('link2')){
-                     value= $('#content .con2').offset().top; 
+                     value= $('#content .con2').offset().top -180; 
                   }else if($(this).hasClass('link3')){
-                     value= $('#content .con3').offset().top; 
+                     value= $('#content .con3').offset().top -180; 
                   }else if($(this).hasClass('link4')){
-                     value= $('#content .con4').offset().top; 
+                     value= $('#content .con4').offset().top -180; 
                   }else if($(this).hasClass('link5')){
-                     value= $('#content .con5').offset().top; 
+                     value= $('#content .con5').offset().top -180; 
                   }else if($(this).hasClass('link6')){
-                     value= $('#content .con6').offset().top; 
+                     value= $('#content .con6').offset().top -180; 
                   }else if($(this).hasClass('link7')){
-                     value= $('#content .con7').offset().top; 
+                     value= $('#content .con7').offset().top -180; 
                   }else if($(this).hasClass('link8')){
-                     value= $('#content .con8').offset().top; 
+                     value= $('#content .con8').offset().top -180; 
                   }else if($(this).hasClass('link9')){
-                     value= $('#content .con9').offset().top; 
+                     value= $('#content .con9').offset().top -180; 
                  }
                 $("html,body").stop().animate({"scrollTop":value},1000);
               });
